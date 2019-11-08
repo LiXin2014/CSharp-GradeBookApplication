1b9448423d1492fc427f1879609e9a31be602861
Xin Li <xili3@microsoft.com>
2019-11-08 15:48:57 -0800

    override rankedgradebook getlettergrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,34 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            int numOfStudents = Students.Count;
+            if (numOfStudents < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            List<double> grades = new List<double>();
+            foreach(Student student in Students)
+            {
+                grades.AddRange(student.Grades);
+            }
+            grades.Sort();
+            grades.Reverse();
+
+            int range = numOfStudents / 5;
+
+            if (averageGrade >= grades[range])
+                return 'A';
+            else if (averageGrade >= grades[range*1])
+                return 'B';
+            else if (averageGrade >= grades[range*2])
+                return 'C';
+            else if (averageGrade >= grades[range*3])
+                return 'D';
+            else
+                return 'F';
+        }
     }
 }
